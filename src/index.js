c5e1dccaf0cdfe17d066d11b77c73fef25be42c2
Matthew Sterling <mjsterling93@gmail.com>
2021-03-13 22:29:29 +1100

    assets added. pieces move correctly. need to add check mechanic and en passant/fix pawns

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { unstable_concurrentAct } from "react-dom/test-utils";
-
 class Game extends React.Component {
   constructor(props) {
     super(props);
@@ -10,27 +9,22 @@ class Game extends React.Component {
       history: [
         {
           squares: [
-            ["B", "R"],
-            ["B", "N"],
-            ["B", "B"],
-            ["B", "Q"],
-            ["B", "K"],
-            ["B", "B"],
-            ["B", "N"],
-            ["B", "R"],
-            ["B", "P"],
-            ["B", "P"],
-            ["B", "P"],
-            ["B", "P"],
-            ["B", "P"],
-            ["B", "P"],
-            ["B", "P"],
-            ["B", "P"],
-            [null, null],
-            [null, null],
-            [null, null],
-            [null, null],
-            ["W", "R"],
+            ["b", "r"],
+            ["b", "n"],
+            ["b", "b"],
+            ["b", "q"],
+            ["b", "k"],
+            ["b", "b"],
+            ["b", "n"],
+            ["b", "r"],
+            ["b", "p"],
+            ["b", "p"],
+            ["b", "p"],
+            ["b", "p"],
+            ["b", "p"],
+            ["b", "p"],
+            ["b", "p"],
+            ["b", "p"],
             [null, null],
             [null, null],
             [null, null],
@@ -58,28 +52,34 @@ class Game extends React.Component {
             [null, null],
             [null, null],
             [null, null],
-            ["W", "P"],
-            ["W", "P"],
-            ["W", "P"],
-            ["W", "P"],
-            ["W", "P"],
-            ["W", "P"],
-            ["W", "P"],
-            ["W", "P"],
-            ["W", "R"],
-            ["W", "N"],
-            ["W", "B"],
-            ["W", "Q"],
-            ["W", "K"],
-            ["W", "B"],
-            ["W", "N"],
-            ["W", "R"],
+            [null, null],
+            [null, null],
+            [null, null],
+            [null, null],
+            [null, null],
+            ["w", "p"],
+            ["w", "p"],
+            ["w", "p"],
+            ["w", "p"],
+            ["w", "p"],
+            ["w", "p"],
+            ["w", "p"],
+            ["w", "p"],
+            ["w", "r"],
+            ["w", "n"],
+            ["w", "b"],
+            ["w", "q"],
+            ["w", "k"],
+            ["w", "b"],
+            ["w", "n"],
+            ["w", "r"],
           ],
         },
       ],
       wIsNext: true,
       turnNumber: 0,
       highlights: Array(64).fill("square"),
+      selected: null,
     };
   }
 
@@ -96,24 +96,19 @@ class Game extends React.Component {
           />
         </div>
         <button onClick={() => this.toggleTurn()} className="toggleturn">
-          {" "}
-          {this.state.wIsNext ? "White's turn" : "Black's turn"}
+          {"Move: "}
+          {history.length}
+          {" - "}
+          {this.state.wIsNext ? "White to move" : "Black to move"}
         </button>
       </div>
     );
   }
 
   toggleTurn() {
-    let whoIsNext = this.state.wIsNext;
-    if (whoIsNext) {
-      this.setState({
-        wIsNext: false,
-      });
-    } else {
-      this.setState({
-        wIsNext: true,
-      });
-    }
+    this.setState({
+      wIsNext: !this.state.wIsNext,
+    });
   }
 
   handleClick(i) {
@@ -121,52 +116,76 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     const highlights = this.state.highlights;
-    const wIsNext = this.state.wIsNext;
-    const t = this.state.wIsNext ? "W" : "B";
-    //if active, clear highlights
+    const t = this.state.wIsNext ? "w" : "b";
+    //if active
     if (highlights.includes("active")) {
-      highlights.fill("square");
+      //move piece
+      if (highlights[i] === "legal" || highlights[i] === "capture") {
+        function active(active) {
+          return active === "active";
+        }
+        let activesq = highlights.findIndex(active);
+        console.log("activesq =", activesq);
+        squares[i] = squares[activesq];
+        squares[activesq] = [null, null];
+        highlights.fill("square");
+        this.setState({
+          history: history.concat([
+            {
+              squares: squares,
+            },
+          ]),
+          turnNumber: history.length,
+          wIsNext: !this.state.wIsNext,
+        });
+        return;
+      } else {
+        //clear highlights
+        highlights.fill("square");
+      }
+      // if square empty return
+      if (!squares[i][0]) {
+        this.setState({
+          highlights: highlights,
+        });
+        return;
+      }
     }
-    // if square empty return
-    if (!squares[i][0]) {
-      console.log("squares i null");
+    //if no active
+    if (!highlights.includes("active")) {
+      //if white clicks on white piece, highlight legal moves
+      if (this.state.wIsNext && squares[i][0] === t) {
+        this.pieceHL(t, i, squares, highlights);
+      }
+      //if black clicks on black piece, highlight legal moves
+      if (!this.state.wIsNext && squares[i][0] === t) {
+        this.pieceHL(t, i, squares, highlights);
+      }
       this.setState({
         highlights: highlights,
       });
       return;
     }
-    //if white clicks on white piece, highlight legal moves
-    if (wIsNext && squares[i][0] === t) {
-      this.pieceHL(t, i, squares, highlights);
-    }
-    //if black clicks on black piece, highlight legal moves
-    if (!wIsNext && squares[i][0] === t) {
-      this.pieceHL(t, i, squares, highlights);
-    }
-    this.setState({
-      highlights: highlights,
-    });
-    return;
   }
 
   pieceHL(t, i, squares, highlights) {
-    if (squares[i][1] === "P") {
+    if (squares[i][1] === "p") {
       this.pawnHL(t, i, squares, highlights);
     }
-    if (squares[i][1] === "R") {
+    if (squares[i][1] === "r") {
       this.rookHL(t, i, squares, highlights);
     }
-    if (squares[i][1] === "N") {
+    if (squares[i][1] === "n") {
       this.knightHL(t, i, squares, highlights);
     }
-    if (squares[i][1] === "B") {
+    if (squares[i][1] === "b") {
       this.bishopHL(t, i, squares, highlights);
     }
-    if (squares[i][1] === "Q") {
+    if (squares[i][1] === "q") {
       this.rookHL(t, i, squares, highlights);
       this.bishopHL(t, i, squares, highlights);
     }
-    if (squares[i][1] === "K") {
+    if (squares[i][1] === "k") {
       this.kingHL(t, i, squares, highlights);
     }
   }
@@ -174,22 +193,41 @@ class Game extends React.Component {
   pawnHL(t, i, squares, highlights) {
     //moves only, no captures yet
     highlights[i] = "active";
-    if (t === "W") {
-      for (
-        let n = i - 8;
-        n > i - 17 && squares[n] && !squares[n][0];
-        n = n - 8
-      ) {
+    if (t === "w") {
+      let n = i - 8;
+      for (n; n > i - 17 && squares[n] && !squares[n][0]; n = n - 8) {
         highlights[n] = "legal";
       }
+      n = i - 9;
+      if (squares[n]) {
+        if (squares[n][0] === "b" && n % 8 !== 7) {
+          highlights[n] = "capture";
+        }
+      }
+      n = i - 7;
+      if (squares[n]) {
+        if (squares[n][0] === "b" && n % 8 !== 0) {
+          highlights[n] = "capture";
+        }
+      }
     }
-    if (t === "B") {
-      for (
-        let n = i + 8;
-        n < i + 17 && squares[n] && !squares[n][0];
-        n = n + 8
-      ) {
+
+    if (t === "b") {
+      let n = i + 8;
+      for (n; n < i + 17 && squares[n] && !squares[n][0]; n = n + 8) {
         highlights[n] = "legal";
+      }
+      n = i + 7;
+      if (squares[n]) {
+        if (squares[n][0] === "w" && n % 8 !== 7) {
+          highlights[n] = "capture";
+        }
+      }
+      n = i + 9;
+      if (squares[n]) {
+        if (squares[n][0] === "w" && n % 8 !== 0) {
+          highlights[n] = "capture";
+        }
       }
     }
   }
@@ -251,16 +289,25 @@ class Game extends React.Component {
   knightHL(t, i, squares, highlights) {
     highlights[i] = "active";
     console.log(i);
-    const nmoves = [-17, -15, -10, -6, 6, 10, 15, 17];
-    const imod = i % 8;
-    for (let n = 0; n < 8; n++) {
+    let nmoves;
+    if (i % 8 === 0) {
+      nmoves = [-15, -6, 6, 15];
+    } else if (i % 8 === 1) {
+      nmoves = [-17, -15, -6, 10, 15, 17];
+    } else if (i % 8 === 6) {
+      nmoves = [-17, -15, -10, 6, 15, 17];
+    } else if (i % 8 === 7) {
+      nmoves = [-17, -10, 10, 17];
+    } else {
+      nmoves = [-17, -15, -10, -6, 6, 10, 15, 17];
+    }
+    for (let n = 0; n < nmoves.length; n++) {
       let e = i + nmoves[n];
       if (squares[e]) {
         if (!squares[e][0]) {
           highlights[e] = "legal";
           console.log(e);
-        }
-        else if (squares[e][0] !== t) {
+        } else if (squares[e][0] !== t) {
           highlights[e] = "capture";
           console.log(e);
         }
@@ -270,147 +317,83 @@ class Game extends React.Component {
 
   bishopHL(t, i, squares, highlights) {
     highlights[i] = "active";
-    //up-left
-    for (let n = i - 9; n % 8 !== 7 && !squares[n]; n = n - 9) {
-      if (squares[n]) {
-        if (!squares[n][0]) {
-          console.log(squares[n]);
-          highlights[n] = "legal";
-        }
-        if (
-          t === "W" &&
-          squares[n][0] === "B" &&
-          !highlights.includes("capture")
-        ) {
-          highlights[n] = "capture";
-        }
-        if (t === "B" && squares[n][0] === "W") {
-          highlights[n] = "capture";
-        }
-      }
+    let n = i - 9; //up-left
+    for (n; squares[n] && !squares[n][0] && n % 8 !== 7; n = n - 9) {
+      highlights[n] = "legal";
     }
-    //up-right
-    for (let n = i - 7; n % 8 !== 0 && !squares[n]; n = n - 7) {
-      if (squares[n]) {
-        if (!squares[n][0]) {
-          console.log(squares[n]);
-          highlights[n] = "legal";
-        }
-        if (
-          t === "W" &&
-          squares[n][0] === "B" &&
-          !highlights.includes("capture")
-        ) {
-          highlights[n] = "capture";
-        }
-        if (t === "B" && squares[n][0] === "W") {
-          highlights[n] = "capture";
-        }
-      }
+    for (
+      n;
+      squares[n] &&
+      squares[n][0] !== t &&
+      highlights[n + 9] !== "capture" &&
+      n % 8 !== 7;
+      n = n - 9
+    ) {
+      highlights[n] = "capture";
     }
-    //down-left
-    for (let n = i + 7; n % 8 !== 7 && !squares[n]; n = n + 7) {
-      if (squares[n]) {
-        if (!squares[n][0]) {
-          console.log(squares[n]);
-          highlights[n] = "legal";
-        }
-        if (
-          t === "W" &&
-          squares[n][0] === "B" &&
-          !highlights.includes("capture")
-        ) {
-          highlights[n] = "capture";
-        }
-        if (t === "B" && squares[n][0] === "W") {
-          highlights[n] = "capture";
-        }
-      }
+    n = i + 9; //down-right
+    for (n; squares[n] && !squares[n][0] && n % 8 !== 0; n = n + 9) {
+      highlights[n] = "legal";
     }
-    //down-right
-    for (let n = i + 9; n % 8 !== 0 && !squares[n]; n = n + 9) {
-      if (squares[n]) {
-        if (!squares[n][0]) {
-          console.log(squares[n]);
-          highlights[n] = "legal";
-        }
-        if (
-          t === "W" &&
-          squares[n][0] === "B" &&
-          !highlights.includes("capture")
-        ) {
-          highlights[n] = "capture";
-        }
-        if (t === "B" && squares[n][0] === "W") {
-          highlights[n] = "capture";
-        }
-      }
+    for (
+      n;
+      squares[n] &&
+      squares[n][0] !== t &&
+      highlights[n - 9] !== "capture" &&
+      n % 8 !== 0;
+      n = n + 9
+    ) {
+      highlights[n] = "capture";
+    }
+    n = i - 7; //up-right
+    for (n; squares[n] && !squares[n][0] && n % 8 !== 0; n = n - 7) {
+      highlights[n] = "legal";
+    }
+    for (
+      n;
+      squares[n] &&
+      squares[n][0] !== t &&
+      n % 8 !== 0 &&
+      highlights[n + 7] !== "capture";
+      n = n - 7
+    ) {
+      highlights[n] = "capture";
+    }
+    n = i + 7; //down-left
+    for (n; squares[n] && !squares[n][0] && n % 8 !== 7; n = n + 7) {
+      highlights[n] = "legal";
+    }
+    for (
+      n;
+      squares[n] &&
+      squares[n][0] !== t &&
+      n % 8 !== 7 &&
+      highlights[n - 9] !== "capture";
+      n = n + 9
+    ) {
+      highlights[n] = "capture";
     }
   }
 
   kingHL(t, i, squares, highlights) {
     highlights[i] = "active";
-    const nmoves = [-9, -8, -7, -1, 100, 1, 7, 8, 9];
-    //a file
-    for (let n = 0; n < 9 && squares[i]; n = n++) {
+    let nmoves;
+    if (i % 8 === 0) {
+      nmoves = [-8, -7, 1, 8, 9];
+    } else if (i % 8 === 7) {
+      nmoves = [-9, -8, -1, 7, 8];
+    } else {
+      nmoves = [-9, -8, -7, -1, 1, 7, 8, 9];
+    }
+    for (let n = 0; n < nmoves.length; n++) {
       let e = i + nmoves[n];
-      if (i % 8 === 0) {
-        if (n % 3 !== 0) {
-          if (squares[e]) {
-            if (!squares[e][0]) {
-              console.log(squares[e]);
-              highlights[e] = "legal";
-            }
-            if (
-              t === "W" &&
-              squares[e][0] === "B" &&
-              !highlights.includes("capture")
-            ) {
-              highlights[e] = "capture";
-            }
-            if (t === "B" && squares[e][0] === "W") {
-              highlights[e] = "capture";
-            }
-          } //h file
-        }
-      } else if (i % 8 === 7) {
-        if (n % 3 !== 2) {
-          if (squares[e]) {
-            if (!squares[e][0]) {
-              console.log(squares[e]);
-              highlights[e] = "legal";
-            }
-            if (
-              t === "W" &&
-              squares[e][0] === "B" &&
-              !highlights.includes("capture")
-            ) {
-              highlights[e] = "capture";
-            }
-            if (t === "B" && squares[e][0] === "W") {
-              highlights[e] = "capture";
-            }
-          }
-        }
-        //other files
-      } else {
-        for (let n = 0; n < 9; ++n) {
-          if (squares[e]) {
-            if (!squares[e][0]) {
-              console.log(squares[e]);
-              highlights[e] = "legal";
-            }
-            if (
-              t === "W" &&
-              squares[e][0] === "B" &&
-              !highlights.includes("capture")
-            ) {
-              highlights[e] = "capture";
-            }
-            if (t === "B" && squares[e][0] === "W") {
-              highlights[e] = "capture";
-            }
-          }
+      if (squares[e]) {
+        if (!squares[e][0]) {
+          highlights[e] = "legal";
+          console.log(e);
+        } else if (squares[e][0] !== t) {
+          highlights[e] = "capture";
+          console.log(e);
         }
       }
     }
@@ -422,9 +405,10 @@ class Board extends React.Component {
     return (
       <Square
         key={i}
-        value={this.props.squares[i]}
+        // value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
-        class={this.props.sqclass[i]}
+        class1={this.props.sqclass[i]}
+        class2={this.props.squares[i][0] + this.props.squares[i][1]}
       />
     );
   }
@@ -439,11 +423,15 @@ class Board extends React.Component {
 }
 
 function Square(props) {
-  return (
-    <button className={props.class} onClick={() => props.onClick()}>
-      {props.value}
-    </button>
-  );
+  if (props.class2) {
+    return (
+      <button className={props.class1}>
+        <div className={props.class2}  onClick={() => props.onClick()} />
+      </button>
+    );
+  } else {
+    return <button className={props.class1} onClick={() => props.onClick()} />;
+  }
 }
 
 // ========================================
